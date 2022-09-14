2d4446da1dc9c00d5c2590d723665a2d31838fbd
JimiSewa <adigunoluwaferanmi2@gmail.com>
2022-09-14 07:53:22 -0700

    put

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,12 @@
 
 /**
 * main - Entry point
-* Return: Always 0 (Success)
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
-{
+(
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
