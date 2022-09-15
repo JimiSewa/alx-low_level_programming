cb0b6548c2b9cb1b37ad8a6a4836d336335ad61b
JimiSewa <adigunoluwaferanmi2@gmail.com>
2022-09-15 16:00:58 -0700

    digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - does what the name says
+ * @num: input being checked
+ * Description: same as above
+ * Return: last digit
+ */
+
+int print_last_digit(int num)
+{
+	num = num % 10;
+
+	if (num < 0)
+		num = -num;
+	_putchar(num + '0');
+	return (num);
+}
