f32b1961ecf021fe552843aa7334c1848dfe9ead
JimiSewa <adigunoluwaferanmi2@gmail.com>
2022-09-16 10:22:14 -0700

    hours

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,5 @@ void print_alphabet_x10(void);
 	int _isalpha(int c);
 	int print_sign(int n);
 	int _abs(int);
-       int print_last_digit(int);       
+       int print_last_digit(int);
+void jack_bauer(void);       
