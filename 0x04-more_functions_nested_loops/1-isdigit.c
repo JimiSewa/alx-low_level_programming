c1b97a082fcfe80cfcdd082e6cc68aade8a9173e
JimiSewa <adigunoluwaferanmi2@gmail.com>
2022-09-18 11:55:19 -0700

    loop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,10 @@
  *Return: 1 if it is, 0 otherwise
  */
 
-int _isdigit(int c)
+int _isdigit(char c)
 {
-	if ((c >= 48) && (c <= 57))
-	{
+	if ((c >= '0') && (c <= '9'))
 		return (1);
-	}
-
-	return (0);
+	else
+		return (0);
 }
