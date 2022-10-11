69aa1f3af6ae508700d24b8e112fd4584b7dbd6c
JimiSewa <adigunoluwaferanmi2@gmail.com>
2022-10-11 08:58:21 -0700

    fhgfhg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: Arguments count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
