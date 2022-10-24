8fe287fb66c26318c0cb2e81a8bc8fff8fd8de3e
JimiSewa <adigunoluwaferanmi2@gmail.com>
2022-10-24 02:31:53 -0700

    dghd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-fndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
 #include <stddef.h>
@@ -21,7 +21,6 @@ struct dog
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
